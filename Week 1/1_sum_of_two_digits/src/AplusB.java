781c205463787f23000f30a8562d3842233d26aa
QuyNguyen <quyquy97@gmail.com>
2021-08-05 21:37:58 +0700

    Edit Week 1 - Assignment 1

--- Week 1/1_sum_of_two_digits/src/AplusB.java
+++ Week 1/1_sum_of_two_digits/src/AplusB.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class AplusB {
+class APlusB {
+    static int sumOfTwoDigits(int first_digit, int second_digit) {
+        return first_digit + second_digit;
+    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-
-        int sum = a + b;
-        System.out.println(sum);
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(sumOfTwoDigits(a, b));
     }
 }
