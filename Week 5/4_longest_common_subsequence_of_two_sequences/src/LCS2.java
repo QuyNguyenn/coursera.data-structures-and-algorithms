ba1c0555e898a723663dab93562e098cc977c190
QuyNguyen <quyquy97@gmail.com>
2021-08-08 15:18:28 +0700

    Modified assignment 4 and 5

--- Week 5/4_longest_common_subsequence_of_two_sequences/src/LCS2.java
+++ Week 5/4_longest_common_subsequence_of_two_sequences/src/LCS2.java
@@ -29,16 +29,17 @@ public class LCS2 {
                 }
             }
         }
-        /*for (int i = 0; i < na; i++) {
-            for (int j = 0; j< nb; j++) {
+        for (int i = 0; i < xMax; i++) {
+            for (int j = 0; j< yMax; j++) {
                 System.out.print(array[i][j] + "\t");
             }
             System.out.println("");
-        }*/
+        }
         return array;
     }
 
     private static int lcs2(long[] a, long[] b, int[][] array, int i, int j) {
+        //System.out.println(i + " " + j);
         if (i == 0 && j == 0) {
             return 0;
         }
@@ -48,6 +49,7 @@ public class LCS2 {
         int lsc4 = 0;
         if (i > 0 && j > 0 && a[i - 1] == b[j - 1] && array[i][j] == array[i - 1][j - 1]) {
             lsc1 = 1 + lcs2(a, b, array, i - 1, j - 1);
+            //System.out.println(a[i - 1] + " " + b[j - 1]);
         }
         if (i > 0 && j > 0 && a[i - 1] != b[j - 1] && array[i][j] == array[i - 1][j - 1] + 1) {
             lsc2 = lcs2(a, b, array, i - 1, j - 1);
@@ -61,7 +63,74 @@ public class LCS2 {
         return Math.max(lsc1, Math.max(lsc2, Math.max(lsc3, lsc4)));
     }
 
+    private static int lcs2(long[] a, long[] b) {
+        int na = a.length + 1;
+        int nb = b.length + 1;
+        int[][] arr = new int[na][nb];
+        for (int i = 0; i < na; i++) {
+            for (int j = 0; j < nb; j++) {
+                if (i == 0 || j == 0) {
+                    arr[i][j] = 0;
+                }
+                else {
+                    if (a[i - 1] == b[j - 1]) {
+                        arr[i][j] = arr[i - 1][j - 1] + 1;
+                    }
+                    else {
+                        arr[i][j] = Math.max(arr[i - 1][j], arr[i][j - 1]);
+                    }
+                }
+            }
+        }
+        /*for (int i = 0; i < na; i++) {
+            for (int j = 0; j< nb; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println("");
+        }*/
+        return arr[na - 1][nb - 1];
+    }
+
     public static void main(String[] args) {
+        /*while (true)
+        {
+            Random random = new Random();
+            int n = random.nextInt(15) + 5;
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = random.nextLong() % 20;
+            }
+
+            int m = random.nextInt(15) + 5;
+            long[] b = new long[m];
+            for (int i = 0; i < m; i++) {
+                b[i] =  random.nextLong() % 20;
+            }
+
+            for (long longs: a) {
+                System.out.print(longs + "\t");
+            }
+            System.out.println("");
+            for (long longs: b) {
+                System.out.print(longs + "\t");
+            }
+            System.out.println("");
+
+            int[][] editDistance = editDistance(a, b);
+            int re0 = lcs2(a, b, editDistance, a.length, b.length);
+            int re1 = lcs2(a, b);
+            System.out.println(re0);
+            System.out.println(re1);
+            if (re0 != re1) {
+                System.out.println("FAIL");
+                break;
+            }
+            else {
+                System.out.println("OK");
+                System.out.println("");
+            }
+        }*/
+
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         long[] a = new long[n];
@@ -85,8 +154,9 @@ public class LCS2 {
         System.out.println("");
         System.out.println("");*/
 
-        int[][] editDistance = editDistance(a, b);
-        System.out.println(lcs2(a, b, editDistance, a.length, b.length));
+        //int[][] editDistance = editDistance(a, b);
+        //System.out.println(lcs2(a, b, editDistance, a.length, b.length));
+        System.out.println(lcs2(a, b));
     }
 }
 
--- Week 5/5_longest_common_subsequence_of_three_sequences/src/LCS3.java
+++ Week 5/5_longest_common_subsequence_of_three_sequences/src/LCS3.java
@@ -2,21 +2,66 @@ import java.util.*;
 
 public class LCS3 {
 
-    private static int[][][] editDistance(long[] a, long[] b, long[] c) {
+    private static int[][] editDistance2(long[] a, long[] b) {
+        int xMax = a.length + 1;
+        int yMax = b.length + 1;
+        int insertion, deletion, substitution, match;
+        int[][] array = new int[xMax][yMax];
+        array[0][0] = 0;
+        for (int i = 1; i < xMax; i++) {
+            array[i][0] = array[i - 1][0] + 1;
+        }
+        for (int i = 1; i < yMax; i++) {
+            array[0][i] = array[0][i - 1] + 1;
+        }
+        for (int i = 1; i < xMax; i++) {
+            for (int j = 1; j < yMax; j++) {
+                insertion = array[i - 1][j] + 1;
+                deletion = array[i][j - 1] + 1;
+                substitution = array[i - 1][j - 1] + 1;
+                match = array[i - 1][j - 1];
+                if (a[i - 1] == b[j - 1])
+                {
+                    array[i][j] = Math.min(insertion, Math.min(deletion, match));
+                }
+                else {
+                    array[i][j] = Math.min(insertion, Math.min(deletion, substitution));
+                }
+            }
+        }
+        /*for (int i = 0; i < na; i++) {
+            for (int j = 0; j< nb; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println("");
+        }*/
+        return array;
+    }
+
+    private static int[][][] editDistance3(long[] a, long[] b, long[] c) {
         int xMax = a.length + 1;
         int yMax = b.length + 1;
         int zMax = c.length + 1;
         int[] edit = new int[8];
+        int[][] tmp;
         int[][][] arr = new int[xMax][yMax][zMax];
-        arr[0][0][0] = 0;
+        tmp = editDistance2(a, b);
+        for (int i = 0; i < xMax; i++) {
+            for (int j = 0; j < yMax; j++) {
+                arr[i][j][0] = tmp[i][j];
+            }
+        }
+        tmp = editDistance2(a, c);
         for (int i = 1; i < xMax; i++) {
-            arr[i][0][0] = arr[i - 1][0][0] + 1;
+            for (int j = 0; j < zMax; j++) {
+                arr[i][0][j] = tmp[i][j];
+            }
         }
+        tmp = editDistance2(b, c);
         for (int i = 1; i < yMax; i++) {
-            arr[0][i][0] = arr[0][i - 1][0] + 1;
-        }
-        for (int i = 1; i < zMax; i++) {
-            arr[0][0][i] = arr[0][0][i - 1] + 1;
+            for (int j = 0; j < zMax; j++) {
+                arr[0][i][j] = tmp[i][j];
+            }
         }
         for (int i = 1; i < xMax; i++) {
             for (int j = 1; j < yMax; j++) {
@@ -52,11 +97,80 @@ public class LCS3 {
         if (i == 0 && j == 0 && k == 0) {
             return 0;
         }
-        int[] ls = {0, 0, 0, 0, 0, 0, 0};
+        int[] ls = {0, 0, 0, 0, 0, 0, 0, 0};
+
+        // Match
         if (i > 0 && j > 0 && k > 0 && a[i - 1] == b[j - 1] && a[i - 1] == c[k - 1] && edit[i][j][k] == edit[i - 1][j - 1][k - 1]) {
             ls[0] = 1 + lcs3(a, b, c, edit, i - 1, j - 1, k - 1);
         }
-        return editDistance(a, b, c)[a.length][b.length][c.length];
+
+        // Substitution
+        if (i > 0 && j > 0 && k > 0 && (a[i - 1] != b[j - 1] || a[i - 1] != c[k - 1]) && edit[i][j][k] == edit[i - 1][j - 1][k - 1] + 1) {
+            ls[1] = lcs3(a, b, c, edit, i - 1, j - 1, k - 1);
+        }
+
+        //
+        if (j > 0 && edit[i][j][k] == edit[i][j - 1][k] + 1) {
+            ls[2] = lcs3(a, b, c, edit, i, j - 1, k);
+        }
+        if (j > 0 && k > 0 && edit[i][j][k] == edit[i][j - 1][k - 1] + 1) {
+            ls[3] = lcs3(a, b, c, edit, i, j - 1, k - 1);
+        }
+        if (k > 0 && edit[i][j][k] == edit[i][j][k - 1] + 1) {
+            ls[4] = lcs3(a, b, c, edit, i, j, k - 1);
+        }
+        if (i > 0 && edit[i][j][k] == edit[i - 1][j][k] + 1) {
+            ls[5] = lcs3(a, b, c, edit, i - 1, j, k);
+        }
+        if (i > 0 && k > 0 && edit[i][j][k] == edit[i - 1][j][k - 1] + 1) {
+            ls[6] = lcs3(a, b, c, edit, i - 1, j, k - 1);
+        }
+        if (i > 0 && j > 0 && edit[i][j][k] == edit[i - 1][j - 1][k] + 1) {
+            ls[7] = lcs3(a, b, c, edit, i - 1, j - 1, k);
+        }
+
+        int max = ls[0];
+        for (int ints: ls) {
+            max = Math.max(max, ints);
+        }
+        return max;
+    }
+
+    private static int lcs3(long[] a, long[] b, long[] c) {
+        int na = a.length + 1;
+        int nb = b.length + 1;
+        int nc = c.length + 1;
+        int[][][] arr = new int[na][nb][nc];
+        int[] ar = {0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < na; i++) {
+            for (int j = 0; j < nb; j++) {
+                for (int k = 0; k < nc; k++) {
+                    if (i == 0 || j == 0 || k == 0) {
+                        arr[i][j][k] = 0;
+                    }
+                    else {
+                        if (a[i - 1] == b[j - 1] && a[i - 1] == c[k - 1]) {
+                            arr[i][j][k] = arr[i - 1][j - 1][k - 1] + 1;
+                        }
+                        else {
+                            ar[0] = arr[i][j - 1][k];
+                            ar[1] = arr[i][j - 1][k - 1];
+                            ar[2] = arr[i][j][k - 1];
+                            ar[3] = arr[i - 1][j][k];
+                            ar[4] = arr[i - 1][j][k - 1];
+                            ar[5] = arr[i - 1][j - 1][k];
+                            int max = ar[0];
+                            for (int ints: ar) {
+                                max = Math.max(max, ints);
+                            }
+                            arr[i][j][k] = max;
+                        }
+                    }
+
+                }
+            }
+        }
+        return arr[na - 1][nb - 1][nc - 1];
     }
 
     public static void main(String[] args) {
@@ -77,8 +191,9 @@ public class LCS3 {
             c[i] = scanner.nextInt();
         }
 
-        int[][][] edit = editDistance(a, b, c);
-        System.out.println(lcs3(a, b, c, edit, an, bn, cn));
+        //int[][][] edit = editDistance3(a, b, c);
+        //System.out.println(lcs3(a, b, c, edit, an, bn, cn));
+        System.out.println(lcs3(a, b, c));
     }
 }
 
